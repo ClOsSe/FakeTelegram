f5a73f8ff9e8272ebc155368eec9a671be6e7ece
mohammad <mohammad@gmail.com>
2021-05-22 03:46:41 -0700

    side panel step one ok

---
+++ components/header/SidePanelItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-list nav dense >
+          <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account-box</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Contacs</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Saved Messages</v-list-item-title>
+            </v-list-item>
+            
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-cog</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Setting</v-list-item-title>
+            </v-list-item>
+            
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account-plus</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>InviteFriends</v-list-item-title>
+            </v-list-item>
+            
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-shield-half-full</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Proxy</v-list-item-title>
+            </v-list-item>
+            
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-help-circle</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Help</v-list-item-title>
+            </v-list-item>
+            
+            <v-list-item  @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+              <v-list-item-icon>
+                <v-icon>mdi-moon-waxing-crescent</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Night Mode</v-list-item-title>
+            </v-list-item>
+            
+          </v-list-item-group>
+        </v-list>
+</template>
--- components/header/index.vue
+++ components/header/index.vue
@@ -1,18 +1,7 @@
 <template>
-  <v-card
-    class="mx-auto overflow-hidden main-header"
-    height="800px"
-    flat
-  >
-    <v-system-bar
-        height="30"
-        color='#1f1f1c'
-    ></v-system-bar>
-    <v-app-bar
-      color="#393A3C"
-      dark
-      flat
-    >
+  <v-card class="mx-auto overflow-hidden main-header" height="800px" flat>
+    <v-system-bar height="30" color='#1f1f1c'></v-system-bar>
+    <v-app-bar color="#393A3C" dark flat>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <template >
         <v-tabs align-with-title color="#fff">
@@ -24,47 +13,61 @@
       </template>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-    <v-card  dark height="150"  class="overflow-hidden ma-0 pa-0 rounded-0">
-      <v-img height="100%"  src="https://picsum.photos/1920/1080?random"></v-img>
-    </v-card>
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+    <div class="user-profile-picter">
+      <v-card  dark height="150"  class="overflow-hidden ma-0 pa-0 rounded-0 ">
+        <v-img height="100%"  src="https://picsum.photos/1920/1080?random"></v-img>
+      </v-card>
+      <div @click="show_useracounts = !show_useracounts" class="user-account-name pl-6 m-auto">
+        <div>
+          <div class="d-flex">
+            <h5 class="justify-start">{{username}}</h5>
+            <v-spacer></v-spacer>
+            <v-icon class="justify-end ml-16 pt-1">mdi-chevron-down</v-icon>
+          </div>
+          <h6>{{user_tell}}</h6>
+        </div>
+      </div>
 
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
+      </div>
+      <v-list  nav danse>
+        <span id="fade" :class="show_useracounts ? 'opacity-100' : 'opacity-0'">
+        <v-list-item-group  v-for="(account,index) in user_acccount" :key="index" :class="['user-accounts',show_useracounts ? 'min-height' : '']" >
+            <v-list-item-icon style="margin:0;" class="ma-0 pa-0">
+                <v-img class="ml-2 user-account-avatars pa-0 ma-0" rounded width="40" height="40" src="https://picsum.photos/1920/1080?random"></v-img>
+                <v-list-item-title class="user-account-username pa-0 ma-0 ml-1">{{account.name}}</v-list-item-title>
             </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
         </v-list-item-group>
+        </span>
       </v-list>
-    </v-navigation-drawer>
+        <SIDEPANELITEMS></SIDEPANELITEMS>
+      </v-navigation-drawer>
   </v-card>
 </template>
 
 <script>
-
+import SIDEPANELITEMS from '@/components/header/SidePanelItem.vue'
 export default({
+  components:{
+    SIDEPANELITEMS
+  },
   data: () => ({
       drawer: false,
       group: null,
+      show_useracounts:false,
+      user_acccount:[
+        {name:'mohammad Goudarzi1'},
+        {name:'mohammad Goudarzi2'},
+        {name:'mohammad Goudarzi3'},
+      ],
+      username:'Mohammad Goudarzi',
+      user_tell:"+98 903 399 2048",
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
     }),
 })
 </script>
@@ -72,5 +75,38 @@ export default({
 .main-header{
     width: 100%;
     max-width: 480px;
+}
+.user-profile-picter{
+  position: relative;
+  .user-account-name{
+    position: absolute;
+    margin-top:-44px;
+    width: 100%;
+    background-image: linear-gradient(to bottom, rgba(255,0,0,0), rgba(0,0,0,0.6));
+  }
+}
+  .user-account-avatars{
+    border-radius: 50%;
+  }
+  .user-account-username{
+    font-family: 16px !important;
+  }
+.user-accounts{
+  max-height: 0px;
+  overflow: hidden;
+  transition-duration: 0.5s;
+}
+.min-height{
+  max-height:50px !important;
+  transition-duration: 0.3s;
+}
+
+.opacity-0{
+    opacity: 0;
+    transition: 0.01s;
+}
+.opacity-100{
+    opacity: 1;
+    transition: 2s;
 }
 </style>
